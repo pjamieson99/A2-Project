809feb3db7ee14ded51b02960d3bbdbd0b25f489
pjamieson99 <32102111+pjamieson99@users.noreply.github.com>
2017-09-28 10:30:18 +0100

    Add files via upload
    made the properties of the objects as the actual properties, still need to make legs stop when touching floor

---
+++ Actual graphics/Form1.vb
@@ -0,0 +1,113 @@
+﻿Public Class Form1
+    Dim line(2) As CLegs
+    Dim P1 As Integer = 100
+    Dim up(2) As Boolean
+    Dim down(2) As Boolean
+    Dim increase(2) As Integer
+
+    Dim floor As CFloor
+    Dim count(2) As Integer
+
+    Dim CheckStatic(2) As Boolean
+    Dim joint(2) As CJoints
+
+    Dim RunOnce As Integer = 0
+
+
+    '  Dim gravity As Integer = 2
+    Dim yspeed(2) As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Display_Paint(sender As Object, e As PaintEventArgs) Handles Display.Paint
+        Dim g As Graphics
+        g = e.Graphics
+        floor = New CFloor(500)
+        floor.draw(g)
+
+        If RunOnce = 0 Then
+
+            For x = 0 To 2
+                up(x) = True
+                down(x) = False
+                count(x) = 0
+                increase(x) = 1
+                yspeed(x) = 0
+                line(x) = New CLegs(P1)
+                joint(x) = New CJoints(P1, 10, 10)
+                P1 += 100
+
+
+            Next
+            P1 -= 300
+        End If
+
+        RunOnce += 1
+
+
+
+
+        For x = 0 To 2
+            yspeed(x) += 1
+            line(x).Ypos += yspeed(x)
+            joint(x).JYpos += yspeed(x)
+
+        Next
+
+
+        'For x = 0 To 2
+        '    line(x).draw(g)
+        'Next
+
+        For x = 0 To 2
+
+            joint(x).draw(g)
+            joint(x).drop(g)
+            If (line(x).Speed * increase(x)) > (line(x).Angle / 2) Then
+                up(x) = False
+                down(x) = False
+                If count(x) <> line(x).clock Then
+                    CheckStatic(x) = True
+                    count(x) += 1
+                Else
+                    count(x) = 0
+                    down(x) = True
+                End If
+            ElseIf (line(x).Speed * increase(x)) < -(line(x).Angle / 2) Then
+                up(x) = False
+                down(x) = False
+                If count(x) <> line(x).clock Then
+                    CheckStatic(x) = True
+                    count(x) += 1
+                Else
+                    count(x) = 0
+                    up(x) = True
+                End If
+            End If
+            If count(x) = line(x).clock + 1 Then
+                count(x) = 0
+            End If
+
+            line(x).rotate(g, up(x), down(x), increase(x))
+
+            If up(x) = True Then
+                increase(x) += 1
+            ElseIf down(x) = True Then
+                increase(x) -= 1
+            End If
+
+        Next
+
+
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Display.Refresh()
+    End Sub
+
+
+End Class
---
+++ Actual graphics/LLegs.vb
@@ -0,0 +1,58 @@
+﻿Public Class CLegs
+
+    Public Xpos As Integer
+    Public Ypos As Integer
+    Public Angle As Integer
+    Public Speed As Integer
+    Public clock As Integer
+    Public rnd As New Random
+    Public Sub New(x As Integer)
+        Xpos = x
+
+
+
+
+
+        Ypos = 100
+        Speed = rnd.Next(2, 5)
+        clock = rnd.Next(10, 70)
+        Angle = rnd.Next(45, 180)
+
+    End Sub
+
+    Public Sub draw(g As Graphics)
+        g.DrawLine(Pens.Black, 0, 0, 0, 100)
+    End Sub
+
+    Public Sub rotate(g As Graphics, Up As Boolean, down As Boolean, increase As Integer)
+        If Up = True Then
+
+            g.TranslateTransform(Xpos, Ypos)
+            g.RotateTransform(Speed * increase)
+            draw(g)
+            g.RotateTransform(-Speed * increase)
+            g.TranslateTransform(-Xpos, -Ypos)
+
+            '          g.RotateTransform(-Speed)
+            'ElseIf down = True Then
+            '    g.TranslateTransform(Xpos, Ypos)
+            '    g.RotateTransform(-Speed)
+
+        ElseIf down = True Then
+            g.TranslateTransform(Xpos, Ypos)
+            g.RotateTransform(Speed * increase)
+            draw(g)
+            g.RotateTransform(-Speed * increase)
+            g.TranslateTransform(-Xpos, -Ypos)
+
+        Else
+            g.TranslateTransform(Xpos, Ypos)
+            g.RotateTransform(Speed * increase)
+            draw(g)
+            g.RotateTransform(-Speed * increase)
+            g.TranslateTransform(-Xpos, -Ypos)
+        End If
+
+    End Sub
+
+End Class
