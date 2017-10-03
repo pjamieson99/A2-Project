aa76ae50d8424b28b78605b68bb68241ae643692
pjamieson99 <32102111+pjamieson99@users.noreply.github.com>
2017-10-03 15:46:36 +0100

    Add files via upload
    unsuccessful checking whether points have hjit ground using rotation matrix - recreate program without using rotation function and just use rotation matrix

---
+++ Graphics_gravity_point_check/actual_graphics_point_check/JJoints.vb
@@ -0,0 +1,24 @@
+﻿Public Class CJoints
+    Private Xpos As Integer
+    Private Ypos As Integer
+    Private width As Integer
+    Private diameter As Integer
+    Public JYpos As Integer
+    Public JXpos As Integer
+
+    Public Sub New(x, w, d)
+        width = w
+        diameter = d
+        JYpos = 100
+        JXpos = x
+    End Sub
+
+    Public Sub draw(g As Graphics)
+        g.FillEllipse(Brushes.Black, JXpos - 5, JYpos - 5, width, diameter)
+    End Sub
+
+    Public Sub drop(g As Graphics)
+        Ypos += 10
+
+    End Sub
+End Class
---
+++ Graphics_gravity_point_check/actual_graphics_point_check/LLegs.vb
@@ -0,0 +1,75 @@
+﻿Public Class CLegs
+
+    Public Xpos As Integer
+    Public Ypos As Integer
+    Public Angle As Integer
+    Public Speed As Integer
+    Public clock As Integer
+    Public p1 As PointF
+    Public p2 As PointF
+
+    Public Sub New(x As Integer, rnd As Random)
+        Xpos = x
+        Ypos = 100
+        p1 = New Point(Xpos, Ypos)
+        p2 = New Point(Xpos, Ypos + 100)
+
+
+
+
+        Speed = rnd.Next(2, 5)
+        clock = rnd.Next(10, 70)
+        Angle = rnd.Next(45, 180)
+
+    End Sub
+
+    Public Sub draw(g As Graphics)
+        g.DrawLine(Pens.Black, 0, 0, 0, 100)
+    End Sub
+
+    Public Sub rotate(g As Graphics, Up As Boolean, down As Boolean, increase As Integer)
+        If Up = True Then
+
+
+            g.TranslateTransform(Xpos, Ypos)
+            g.RotateTransform(Speed * increase)
+            draw(g)
+            g.RotateTransform(-Speed * increase)
+            g.TranslateTransform(-Xpos, -Ypos)
+
+            '          g.RotateTransform(-Speed)
+            'ElseIf down = True Then
+            '    g.TranslateTransform(Xpos, Ypos)
+            '    g.RotateTransform(-Speed)
+
+        ElseIf down = True Then
+
+            g.TranslateTransform(Xpos, Ypos)
+            g.RotateTransform(Speed * increase)
+            draw(g)
+            g.RotateTransform(-Speed * increase)
+            g.TranslateTransform(-Xpos, -Ypos)
+
+        Else
+            g.TranslateTransform(Xpos, Ypos)
+            g.RotateTransform(Speed * increase)
+            draw(g)
+            g.RotateTransform(-Speed * increase)
+            g.TranslateTransform(-Xpos, -Ypos)
+        End If
+
+    End Sub
+
+    Sub NewPoints(increase As Integer, floor As CFloor)
+        p1.X = ((p1.X - Xpos) * Math.Cos(Speed * increase * Math.PI / 180)) + ((p1.Y - Ypos) * Math.Sin(Speed * increase * Math.PI / 180)) + Xpos
+        p1.Y = ((p1.X - Xpos) * -Math.Sin(Speed * increase * Math.PI / 180)) + ((p1.Y - Ypos) * Math.Cos(Speed * increase * Math.PI / 180)) + Ypos
+        p2.X = ((p2.X - Xpos) * Math.Cos(Speed * increase * Math.PI / 180)) + ((p2.Y - Ypos) * Math.Sin(Speed * increase * Math.PI / 180)) + Xpos
+        p2.Y = ((p2.X - Xpos) * -Math.Sin(Speed * increase * Math.PI / 180)) + ((p2.Y - Ypos) * Math.Cos(Speed * increase * Math.PI / 180)) + Ypos
+        If floor.ypos < (p1.Y + 50) Or floor.ypos < (p2.Y - 50) Then
+            Console.ReadLine()
+        End If
+    End Sub
+
+
+
+End Class
