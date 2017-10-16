9216876e57f07726a41ae31eba5992d599a5f58a
pjamieson99 <32102111+pjamieson99@users.noreply.github.com>
2017-10-16 16:57:04 +0100

    Add files via upload

---
+++ redone project_cleaner/redone project_cleaner/CBody.vb
@@ -0,0 +1,84 @@
+﻿Public Class CBody
+    Private Bxpos1
+    Private Bxpos2
+    Public Bypos1
+    Public Bypos2
+    Public CoM As PointF
+    Public yspeed As Integer
+    Public connections(2) As PointF
+    Public Hypotenuse As Double
+    Private AngleIncrease As Integer = 0
+    Private py1 As Double
+    Private py2 As Double
+    Private px1 As Double
+    Private px2 As Double
+    Private p1 As PointF
+    Private p2 As PointF
+    Public Sub New(x1, y1, x2, y2)
+
+        Bypos1 = y1
+        Bxpos1 = x1
+        Bypos2 = y2
+        Bxpos2 = x2
+        CoM = New Point((x1 + x2) / 2, (y1 + y2) / 2)
+        yspeed = 0
+        p1 = New Point(x1, y1)
+        p2 = New Point(x2, y2)
+        px1 = x1
+        px2 = x2
+        py1 = y1
+        py2 = y2
+
+
+    End Sub
+
+    Sub BodyPoints(P1Legs() As PointF, GroundLegs As Integer)
+        For x = 0 To GroundLegs
+            connections(x) = P1Legs(x)
+        Next
+    End Sub
+
+    Sub FallRight(pivot As PointF, g As Graphics)
+        AngleIncrease += 1
+        For x = 0 To 2
+            connections(x).X = ((connections(x).X - pivot.X) * Math.Cos((10) * AngleIncrease * Math.PI / 180)) + ((connections(x).Y - pivot.Y) * Math.Sin((10) * Math.PI / 180)) + pivot.X
+            connections(x).Y = ((connections(x).X - pivot.X) * (-Math.Sin((10) * AngleIncrease * Math.PI / 180))) + ((connections(x).Y - pivot.Y) * Math.Cos((10) * Math.PI / 180)) + pivot.Y
+        Next
+        px1 = ((Bxpos1 - pivot.X) * Math.Cos((10) * AngleIncrease * Math.PI / 180)) + ((Bypos1 - pivot.Y) * Math.Sin((10) * AngleIncrease * Math.PI / 180)) + pivot.X
+        py1 = ((Bxpos1 - pivot.Y) * (-Math.Sin((10) * AngleIncrease * Math.PI / 180))) + ((Bypos1 - pivot.Y) * Math.Cos((10) * AngleIncrease * Math.PI / 180)) + pivot.Y
+        px2 = ((Bxpos2 - pivot.X) * Math.Cos((10) * AngleIncrease * Math.PI / 180)) + ((Bypos2 - pivot.Y) * Math.Sin((10) * AngleIncrease * Math.PI / 180)) + pivot.X
+        py2 = ((Bxpos2 - pivot.Y) * (-Math.Sin((10) * AngleIncrease * Math.PI / 180))) + ((Bypos2 - pivot.Y) * Math.Cos((10) * AngleIncrease * Math.PI / 180)) + pivot.Y
+
+
+    End Sub
+
+    Public Sub draw(g As Graphics)
+        p1.Y = py1
+        p1.X = px1
+        p2.Y = py2
+        p2.X = px2
+        g.DrawLine(Pens.Black, p1.X, p1.Y, p2.X, p2.Y)
+        p1.X = Bxpos1
+        p1.Y = Bypos1
+        p2.X = Bxpos2
+        p2.Y = Bypos2
+
+    End Sub
+
+    Public Sub drop()
+        'add speed to fall
+        Bypos1 += yspeed
+        Bypos2 += yspeed
+
+        For x = 0 To 2
+            connections(x).Y += yspeed
+        Next
+        py1 += yspeed
+        py2 += yspeed
+        yspeed += 1
+    End Sub
+
+
+
+
+End Class
---
+++ redone project_cleaner/redone project_cleaner/Form1.vb
@@ -0,0 +1,167 @@
+﻿Public Class Form1
+    Dim GroundLegs As Integer = 2
+    Dim Layers As Integer = 1
+    Dim Line(GroundLegs, Layers) As CLeg
+    Dim floor As CFloor
+    Dim Rnd As New Random
+    Dim joint(GroundLegs, Layers) As CJoint
+    Dim P1 As Integer = 100
+    Dim down As Boolean = False
+    Dim Up As Boolean = True
+    Dim Body As CBody
+    Dim P1Legs(GroundLegs) As PointF
+    Dim LineDiff As Double
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'create floor object
+        floor = New CFloor(500)
+        Body = New CBody(100, 0, 300, 0)
+        'create legs and joints
+        For y = 0 To Layers
+            If y = 1 Then
+                P1 -= (GroundLegs + 1) * 100
+            End If
+            For x = 0 To GroundLegs
+                If y = 1 Then
+                    Line(x, y) = New CLeg(P1, 0, Rnd)
+                    joint(x, y) = New CJoint(P1, 0, 10, 10, 1)
+                    P1Legs(x) = Line(x, y).Lp1
+                Else
+                    Line(x, y) = New CLeg(P1, 100, Rnd)
+                    joint(x, y) = New CJoint(P1, 100, 10, 10, 1)
+                End If
+
+                P1 += 100
+            Next
+
+
+
+        Next
+        P1 -= 300
+        Body.BodyPoints(P1Legs, GroundLegs)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'keep display refreshing, loop through display
+        Display.Refresh()
+    End Sub
+
+    Private Sub Display_Paint(sender As Object, e As PaintEventArgs) Handles Display.Paint
+        Dim g As Graphics
+        g = e.Graphics
+
+        'draw the floor
+        floor.draw(g)
+        Body.drop()
+        For y = 0 To Layers
+            For x = 0 To GroundLegs
+                Line(x, y).AngleLock(floor)
+                Line(x, y).NewPoints()
+            Next
+        Next
+
+
+        For x = 0 To 2
+
+            If floor.ypos <= Line(x, 0).Lpy1 Or floor.ypos <= Line(x, 0).Lpy2 Then
+
+
+                Line(x, 1).Lpy1 -= (Line(x, 0).Lpy2 - floor.ypos)
+                Line(x, 1).LYpos -= (Line(x, 0).Lpy2 - floor.ypos)
+                Line(x, 1).Lpy2 -= (Line(x, 0).Lpy2 - floor.ypos)
+                Body.Bypos1 -= (Line(x, 0).Lpy2 - floor.ypos)
+                Body.Bypos2 -= (Line(x, 0).Lpy2 - floor.ypos)
+                For y = 0 To GroundLegs
+                    Body.connections(y).Y -= (Line(x, 0).Lpy2 - floor.ypos)
+                Next
+
+            End If
+        Next
+        For y = 0 To Layers
+            For x = 0 To GroundLegs
+                Line(x, y).HitFloor(floor)
+            Next
+        Next
+
+
+
+
+            For y = 0 To Layers
+            For x = 0 To GroundLegs
+                '  Line(x, y).HitFloor(floor)
+                If Line(x, y).CheckFloor(floor) = True Then
+                    Body.yspeed = 0
+                    End If
+
+
+            Next
+        Next
+
+        'check which side leg is on when on floor
+        For x = 0 To GroundLegs
+            If Line(x, 0).CheckFloor(floor) = True And Line(x, 0).Lpx1 < Body.CoM.X Then
+                Line(x, 0).LeftSide = True
+            ElseIf Line(x, 0).CheckFloor(floor) = True And Line(x, 0).lpx1 > Body.CoM.X Then
+                Line(x, 0).RightSide = True
+            ElseIf Line(x, 0).CheckFloor(floor) = True And Line(x, 0).lpx1 = Body.CoM.X Then
+                Line(x, 0).RightSide = True
+                Line(x, 0).LeftSide = True
+            Else
+                Line(x, 0).LeftSide = False
+                Line(x, 0).RightSide = False
+            End If
+        Next
+
+        'find pivot point and fall of it
+        For x = GroundLegs To 0 Step -1
+            If Line(x, 0).LeftSide = True And Line(x, 0).RightSide = False Then
+                ' Body.FallRight(Line(x, 1).Lp1, g)
+            ElseIf Line(x, 0).LeftSide = True And Line(x, 0).RightSide = True Then
+
+            End If
+        Next
+
+        'find pivot point and fall of it
+        For x = 0 To GroundLegs
+            If Line(x, 0).LeftSide = False And Line(x, 0).RightSide = True Then
+
+            ElseIf Line(x, 0).LeftSide = True And Line(x, 0).RightSide = True Then
+
+            End If
+        Next
+
+
+
+        'set points to body 
+        For x = 0 To GroundLegs
+            Line(x, 1).Lp2.Y += Body.connections(x).Y - Line(x, 1).Lp1.Y
+            Line(x, 1).Lpy2 += Body.connections(x).Y - Line(x, 1).Lp1.Y
+            Line(x, 1).Lp1 = Body.connections(x)
+            Line(x, 1).LYpos = Line(x, 1).Lp1.Y
+            Line(x, 1).Lpy1 = Line(x, 1).Lp1.Y
+        Next
+        'attach the bottom legs to the top legs and move with them
+        For x = 0 To 2
+            Line(x, 0).Lpx2 += Line(x, 1).Lpx2 - Line(x, 0).Lpx1
+            Line(x, 0).Lpx1 = Line(x, 1).Lpx2
+            Line(x, 0).Lpy2 += Line(x, 1).Lpy2 - Line(x, 0).Lpy1
+            Line(x, 0).Lpy1 = Line(x, 1).Lpy2
+        Next
+        For y = 0 To Layers
+            For x = 0 To GroundLegs
+
+                Line(x, y).draw(g, floor)
+                joint(x, y).StickToLeg(Line(x, y).Lp1)
+                joint(x, y).draw(g)
+            Next
+        Next
+        Body.draw(g)
+
+
+    End Sub
+
+
+
+
+End Class
