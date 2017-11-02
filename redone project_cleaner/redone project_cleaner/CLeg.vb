330fd1f18c495da8b6d4550b05749eeaee8f72b5
pjamieson99 <32102111+pjamieson99@users.noreply.github.com>
2017-11-02 16:00:26 +0000

    Add files via upload
    woreking rotation

---
+++ redone project_cleaner/redone project_cleaner/CLeg.vb
@@ -0,0 +1,111 @@
+﻿Public Class CLeg
+    Public LXpos As Integer
+    Public LYpos As Integer
+    Public Angle As Integer
+    Public Speed As Integer
+    Public clock As Integer
+    Public Lp1 As PointF
+    Public Lp2 As PointF
+    Public Lpx1 As Double
+    Public Lpx2 As Double
+    Public Lpy1 As Double
+    Public Lpy2 As Double
+    Public increase As Integer
+    Public GoThroughClock As Integer
+    Public up As Boolean = True
+    Public down As Boolean = False
+    Public LeftSide As Boolean
+    Public RightSide As Boolean
+
+
+    Public Sub New(x As Integer, y As Integer, rnd As Random)
+        'set properties
+        LXpos = x
+        LYpos = y
+        Lpx1 = LXpos
+        Lpy1 = LYpos
+        Lpx2 = LXpos
+        Lpy2 = LYpos + 100
+        Lp1 = New Point(LXpos, LYpos)
+        Lp2 = New Point(LXpos, LYpos + 100)
+        Speed = rnd.Next(1, 10)
+        clock = rnd.Next(1, 50)
+        Angle = rnd.Next(45, 170)
+        GoThroughClock = 0
+
+    End Sub
+
+    Public Sub draw(g As Graphics, floor As CFloor)
+        'draw object
+        Lp1.X = Lpx1
+        Lp1.Y = Lpy1
+        Lp2.X = Lpx2
+        Lp2.Y = Lpy2
+        g.DrawLine(Pens.Black, Lp1.X, Lp1.Y, Lp2.X, Lp2.Y)
+        'reset points to what the original dshape is but in its current position to allow the rotation matrix to rotate from original angle
+
+
+
+
+
+    End Sub
+
+    Sub NewPoints()
+        Lp1.X = LXpos
+        Lp1.Y = LYpos
+        Lp2.X = LXpos
+        Lp2.Y = LYpos + 100
+        If up = True Then
+            'rotate points using rotation matrix
+            increase += 1
+            Lpx1 = ((Lp1.X - LXpos) * Math.Cos(Speed * increase * Math.PI / 180) + ((Lp1.Y - LYpos) * Math.Sin(Speed * increase * Math.PI / 180))) + LXpos
+            Lpy1 = ((Lp1.X - LXpos) * (-Math.Sin(Speed * increase * Math.PI / 180)) + ((Lp1.Y - LYpos) * Math.Cos(Speed * increase * Math.PI / 180))) + LYpos
+            Lpx2 = ((Lp2.X - LXpos) * Math.Cos(Speed * increase * Math.PI / 180) + ((Lp2.Y - LYpos) * Math.Sin(Speed * increase * Math.PI / 180))) + LXpos
+            Lpy2 = ((Lp2.X - LXpos) * (-Math.Sin(Speed * increase * Math.PI / 180)) + ((Lp2.Y - LYpos) * Math.Cos(Speed * increase * Math.PI / 180))) + LYpos
+
+        ElseIf down = True Then
+            increase -= 1
+            Lpx1 = ((Lp1.X - LXpos) * Math.Cos(Speed * increase * Math.PI / 180) + ((Lp1.Y - LYpos) * Math.Sin(Speed * increase * Math.PI / 180))) + LXpos
+            Lpy1 = ((Lp1.X - LXpos) * (-Math.Sin(Speed * increase * Math.PI / 180)) + ((Lp1.Y - LYpos) * Math.Cos(Speed * increase * Math.PI / 180))) + LYpos
+            Lpx2 = ((Lp2.X - LXpos) * Math.Cos(Speed * increase * Math.PI / 180) + ((Lp2.Y - LYpos) * Math.Sin(Speed * increase * Math.PI / 180))) + LXpos
+            Lpy2 = ((Lp2.X - LXpos) * (-Math.Sin(Speed * increase * Math.PI / 180)) + ((Lp2.Y - LYpos) * Math.Cos(Speed * increase * Math.PI / 180))) + LYpos
+
+        End If
+
+    End Sub
+
+    Sub AngleLock(floor As CFloor)
+        If Angle / 2 <= Speed * increase Then
+            GoThroughClock += 1
+            If GoThroughClock >= clock Then
+                GoThroughClock = 0
+                down = True
+                up = False
+            Else
+                down = False
+                up = False
+            End If
+        ElseIf Angle / 2 <= Speed * -increase And increase < 0 Then
+            GoThroughClock += 1
+            If GoThroughClock >= clock Then
+                GoThroughClock = 0
+                down = False
+                up = True
+            Else
+                down = False
+                up = False
+            End If
+        End If
+    End Sub
+
+
+    Function CheckFloor(floor As CFloor)
+        If floor.ypos <= Lpy1 Or floor.ypos <= Lpy2 Then
+            Return True
+        Else
+            Return False
+
+        End If
+    End Function
+
+End Class
---
+++ redone project_cleaner/redone project_cleaner/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Display = New System.Windows.Forms.PictureBox()
+        CType(Me.Display, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 40
+        '
+        'Display
+        '
+        Me.Display.BackColor = System.Drawing.Color.White
+        Me.Display.Location = New System.Drawing.Point(28, 12)
+        Me.Display.Name = "Display"
+        Me.Display.Size = New System.Drawing.Size(1011, 517)
+        Me.Display.TabIndex = 0
+        Me.Display.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(1079, 553)
+        Me.Controls.Add(Me.Display)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Display, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Display As PictureBox
+End Class
